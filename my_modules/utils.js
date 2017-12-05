e66643c0307b7e51725c90e5530735b0803261ce
hvail <415195993@qq.com>
2017-12-05 16:18:34 +0800

    v1.8.11(添加webhook操作日志)

--- my_modules/utils.js
+++ my_modules/utils.js
@@ -191,7 +191,7 @@ router.ClassClone = function (src, tar, res) {
 router.DoPushPost = function (url, data, cb, log) {
     request(getHttpOptions(url, data), function (err, res, body) {
         if (url.indexOf("sky1088") < 0 || log) {
-            var msg = new Date().FormatDate() + " , " + url + " , " + res.statusCode + " (" + JSON.stringify(body) + ") INFO : " + JSON.stringify(data);
+            var msg = url + " , " + res.statusCode + " (" + JSON.stringify(body) + ") INFO : " + JSON.stringify(data);
             logger.info(msg);
         }
         cb && cb(url, data, res.statusCode < 400 ? 1 : -1, body);
