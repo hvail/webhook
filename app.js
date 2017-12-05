7c74b540e3e30f333f4abbca83b9e05612b026ec
hvail <415195993@qq.com>
2017-12-05 15:15:15 +0800

    v1.8.6(添加webhook操作日志)

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'ejs');
 
 log4js.configure({
     appenders: {cheese: {type: 'file', filename: 'cheese.log'}},
-    categories: {default: {appenders: ['cheese'], level: 'error'}},
+    categories: {default: {appenders: ['cheese'], level: 'info'}},
     replaceConsole: true
 });
 
