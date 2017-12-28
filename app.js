68da3c4bc607140049d2618c1e3a8adf8e2ba5c1
hvail <415195993@qq.com>
2017-12-28 16:46:26 +0800

    v1.11.5-添加日志

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ let logger = log4js.getLogger('normal');
 // logger.setLevel('DEBUG')
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
