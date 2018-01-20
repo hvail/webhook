8a34cf92cb04f8fa5fc11b7738b89081c58c71d9
hvail <415195993@qq.com>
2018-01-20 12:15:53 +0800

    v1.14.7-围栏数据的本地存储

--- routes/fence.js
+++ routes/fence.js
@@ -71,7 +71,7 @@ let _readLastAndSet = function (sn, poi, cb) {
     redis.hget(key_poi_calc, sn, function (err, rs) {
         err && console.log(err);
         if (rs !== null && rs[0] === '{') {
-            let obj = JOSN.parse(rs);
+            let obj = JSON.parse(rs);
             cb && cb([obj]);
         } else cb && cb(null);
         redis.hset(key_poi_calc, sn, JSON.stringify(poi));
