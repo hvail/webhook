676ef2b92cc53388d86bc9810e4ba5e8f801d984
hvail <415195993@qq.com>
2018-01-25 09:25:14 +0800

    v1.16-里程的Redis计算方式

--- routes/network.js
+++ routes/network.js
@@ -5,9 +5,16 @@
 const express = require('express');
 const router = express.Router();
 
+let _doPost = function (req, res, next) {
+    console.log(req.body);
+    res.status(200).send('1');
+};
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
+
+router.post('/', _doPost);
 
 module.exports = router;
