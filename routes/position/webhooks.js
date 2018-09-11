4e3dd427dab403ac94e855c08bbdaac1e7136e8d
hvail <415195993@qq.com>
2018-09-11 17:59:31 +0800

    v2.8.2.2-Webhooks

--- routes/position/webhooks.js
+++ routes/position/webhooks.js
@@ -10,13 +10,29 @@ const router = express.Router();
 
 const getWebhookUrl = `http://v3.manager-redis.server.${area}.sky1088.com/sales/unit-group-hooks/field/Position`
 
+/**
+ * [{"Url":"http://gps2.bagomart.com/mobile/mb_gps_trajectory.php"}]
+ */
+
+let doWebPush = function (arr, data) {
+    for (let i = 0; i < arr.length; i++)
+        for (let j = 0; j < data.length; j++) {
+            console.log(arr[i]);
+            console.log(data[j]);
+            // apiUtil.PromisePost();
+        }
+};
+
 const _location = (req, res, next) => {
     let pos = req.body;
     let _pos = pos.filter(p => p !== "null");
     let sn = _pos[0].SerialNumber;
 
     let url = `${getWebhookUrl}/${sn}`;
-    apiUtil.PromiseGet(url).then(msg => console.log(`${url} ==> ${msg}`)).catch(e => console.log(e));
+    apiUtil.PromiseGet(url)
+        .then(JSON.parse)
+        .then(arr => (arr && arr.length) && doWebPush(arr, _pos))
+        .catch(e => console.log(e));
 
     next();
 };
