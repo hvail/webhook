3a0b84fdac4cae09ebce6abca69f78fc6b4d69a0
hvail <415195993@qq.com>
2018-12-28 16:38:57 +0800

    v3.1.0.32

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.send("version 3.1.0.31 : " + (process.env.Version || "0000"))
+    res.send("version 3.1.0.32 : " + (process.env.Version || "0000"))
 });
 
 module.exports = router;
