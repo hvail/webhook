60ddf753fb02a603f5bf23bc089a21364f83094b
hvail <415195993@qq.com>
2019-05-07 12:48:28 +0800

    2.9.1-添加里程处理中的过滤

--- bin/listen.js
+++ bin/listen.js
@@ -13,12 +13,13 @@ const host = _env.MQ_RABBIT_HOST || "119.23.27.9", name = _env.MQ_RABBIT_NAME ||
 
 let listenMsg = (msg, type) => {
     let arr = JSON.parse(msg.content.toString());
-    apiUtil.PromisePost('http://core.mileage.sky1088.com/mileage', arr);
-    // .then(msg => console.log('http://core.mileage.sky1088.com/mileage :: ' + msg))
+    apiUtil.PromisePost('http://core.mileage.sky1088.com/mileage', arr)
+        .then(msg => console.log('http://core.mileage.sky1088.com/mileage :: ' + msg));
 };
 
 let listenMq = (custom) => {
     custom.listen("hyz.fanout.position", "run.mileage", msg => listenMsg(msg, "position"));
+    console.log("队列服务连接成功");
 };
 
 let MQ = new mqClient(host, name, pwd, listenMq);
