4faeb03321f57fae8eebf02eb614364aaf52556c
415195993@qq.com <415195993@qq.com>
2020-03-12 15:27:41 +0800

    ZCYX专用版

--- routes/event/webhooks.js
+++ routes/event/webhooks.js
@@ -4,8 +4,10 @@
 let express = require('express');
 let request = require('request');
 let {util: apiUtil} = require('api-base-hvail');
+const log4js = require('log4js');
 let router = express.Router();
 
+let logger = log4js.getLogger('normal');
 const getWebhookUrl = `http://dealer.support.sky1088.com/device/push/Event`;
 
 let getDemo = function (req, res, next) {
@@ -15,7 +17,7 @@ let getDemo = function (req, res, next) {
 const doWebPush = function (url, data) {
     for (let j = 0; j < data.length; j++) {
         apiUtil.PromisePost(url, data[j])
-            .then(ss => console.log(`${url} , 200 (${JSON.stringify(ss)}) INFO : (${JSON.stringify(data[j])})`))
+            .then(ss => logger.info(`${url} , 200 (${JSON.stringify(ss)}) INFO : (${JSON.stringify(data[j])})`))
             .catch(e => console.log(url + ":" + e));
     }
 };
